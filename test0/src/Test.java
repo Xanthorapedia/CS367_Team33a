eaa79d9ae5bc230ceae7a1a6604aec8437f8c4a3
gds12 <gds12@Gao-PC>
2017-02-04 15:36:54 -0600

    6448

--- test0/src/Test.java
+++ test0/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(euclid(6, 4));
+		System.out.println(euclid(64, 48));
 	}
 	
 	public static int euclid(int x, int y) {
