389d5ca20b76a679e1c3bfdefd065fd5b4d07ff5
gds12 <gds12@Gao-PC>
2017-02-04 13:33:26 -0600

    test

---
+++ test0/src/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(euclid(3, 1));
+	}
+	
+	public static int euclid(int x, int y) {
+		if (x < y) {
+			int tmp = x;
+			x = y;
+			y = tmp;
+		}
+		// x >= y
+		
+		while(y > 0) {
+			int tmp = x % y;
+			x = y;
+			y = tmp;
+		}
+		return x;
+	}
+}
