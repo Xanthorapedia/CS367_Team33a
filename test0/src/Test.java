5a82a3e99fde2475113b05ba8d3101c9563b8bac
TG55AA <gds1226@hotmail.com>
2017-02-04 15:36:17 -0600

    Update Test.java

--- test0/src/Test.java
+++ test0/src/Test.java
@@ -2,10 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(euclid(6, 4));
+		System.out.println(euclid1(6, 4));
 	}
 	
-	public static int euclid(int x, int y) {
+	public static int euclid1(int x, int y) {
 		if (x < y) {
 			int tmp = x;
 			x = y;
