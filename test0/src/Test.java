14a52903e3143caf94d504b5d1db8ce10fb4c514
tg55aa <gds1226@hotmail.com>
2017-02-04 21:02:01 -0600

    k

--- test0/src/Test.java
+++ test0/src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 	public static void main(String[] arg) {
 		System.out.println("???");
-		System.out.println("123!");
+		System.out.println("KJ!");
+		System.out.println("345");
 	}
 }
