13e772a49c8811031a4e02fe087a555b2fa0ef2e
tg55aa <gds1226@hotmail.com>
2017-02-11 22:50:10 -0600

    comment

--- Program1/src/GradeEstimator.java
+++ Program1/src/GradeEstimator.java
@@ -5,20 +5,19 @@
 //
 // TEAM:    team 33
 // Authors: team 33 members
-// Author1: (Dasong Gao,dgao24@wisc.edu,dgao24,lec001)
-// Author2: (Sam Ruh,sruh@wisc.edu,sruh,lec001)
-// Author3: (Maggie Buday,Mbuday@wisc.edu,mbuday,lec001)
-// Author4: (Bobby Lv,zlv7,zlv7@wisc.edu,lec001)
-// Author5: (Meredith Lou,ylou9@wisc.edu,ylou9,lec001)
-// Author6: (Apoorva Dhawan,dhawan3@wisc.edu,dhawan3,lec001)
+// Author1: Dasong Gao,		dgao24@wisc.edu,	dgao24,		lec001
+// Author2: Sam Ruh,		sruh@wisc.edu,		sruh,		lec001
+// Author3: Maggie Buday,	Mbuday@wisc.edu,	mbuday,		lec001
+// Author4: Bobby Lv,		zlv7@wisc.edu,		zlv7,		lec001
+// Author5: Meredith Lou,	ylou9@wisc.edu,		ylou9,		lec001
+// Author6: Apoorva Dhawan,	dhawan3@wisc.edu,	dhawan3,	lec001
 //
 // ---------------- OTHER ASSISTANCE CREDITS 
-// Persons: Identify persons by name, relationship to you, and email. 
-// Describe in detail the the ideas and help they provided. 
+// Persons: none.
 // 
-// Online sources: avoid web searches to solve your problems, but if you do 
-// search, be sure to include Web URLs and description of 
-// of any information you find. 
+// Online sources: 
+//		Pattern class usage:
+//		https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
 //////////////////////////// 80 columns wide //////////////////////////////////
 import java.io.FileNotFoundException;
 import java.io.File;
