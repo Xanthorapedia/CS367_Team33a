9cb0d1c3a08a00c656df26b31e2db0c1737f62b3
tg55aa <gds1226@hotmail.com>
2017-02-18 15:42:53 -0600

    iter

--- Program2/src/JobListIterator.java
+++ Program2/src/JobListIterator.java
@@ -1,17 +1,55 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+/**
+ * This class implements the Job iterator for the JobList Class.
+ * The remove() method is not used.
+ * @author Dasong Gao
+ *
+ */
 public class JobListIterator implements Iterator<Job> {
-
-	@Override
-	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+	/** the header node of the Job list*/
+	Listnode<Job> header;
+	/** the current Listnode*/
+	Listnode<Job> cur;
+	
+	/**
+	 * The constructor creates a iterator of JobList.
+	 * @param header - the header node of the list
+	 */
+	public JobListIterator(Listnode<Job> header) {
+		this.cur = this.header = header;
 	}
 
+	/**
+	 * The method checks if there is a next element in the list.
+	 * @return true if there is a next element, false if not.
+	 */
+	@Override
+	public boolean hasNext() {
+		return cur.getNext() != null;
+	}
+
+	/**
+	 * The method returns the next element of the current one.
+	 * @return next Job element in the list.
+	 * @throws NoSuchElementException if the iteration has no more Job elements
+	 */
 	@Override
 	public Job next() {
-		// TODO Auto-generated method stub
-		return null;
+		if (!this.hasNext())
+			throw new NoSuchElementException();
+		cur = cur.getNext();
+		return cur.getData();
+	}
+	
+	/**
+	 * The remove() method is not used.
+	 * @throws UnsupportedOperationException when the method called.
+	 */
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
 	}
 
 }
