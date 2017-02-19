c794ef030f907ff33948294e46095289bb417e81
Meredith <Yafeilou@10.140.150.225>
2017-02-18 18:25:03 -0600

    Upload

--- Program2/src/JobList.java
+++ Program2/src/JobList.java
@@ -2,52 +2,124 @@ import java.util.Iterator;
 
 public class JobList implements ListADT<Job> {
 
+	private Listnode<Job> head; 
+	private int numItems;
+
+	public JobList(){
+		head = new Listnode<Job>(null);
+		numItems =0; 
+
+	}
+
 	@Override
 	public Iterator<Job> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new JobListIterator(head);
 	}
 
 	@Override
 	public void add(Job item) {
-		// TODO Auto-generated method stub
-		
+		if(item == null)
+			throw new IllegalArgumentException();
+		Listnode<Job> newnode = new Listnode<Job>(item);
+		Listnode<Job> node = head;
+		while(node.getNext() != null)	
+			node = node.getNext();
+		node.setNext(newnode);
+
+		numItems++;
+
 	}
 
 	@Override
 	public void add(int pos, Job item) {
-		// TODO Auto-generated method stub
-		
+
+		if(item == null){
+			throw new IllegalArgumentException();
+		}
+
+		if(pos < 0 || pos >= numItems){
+			throw new IndexOutOfBoundsException();
+		}
+
+		Listnode<Job> newNode = new Listnode<Job>(item);
+
+		Listnode<Job> cur = head;
+		for(int curr = 0; curr < pos; curr++){
+			cur = cur.getNext();
+		}
+		newNode.setNext(cur.getNext());
+		cur.setNext(newNode);
+
+
+
+		numItems++;
+
+
+
 	}
 
 	@Override
 	public boolean contains(Job item) {
-		// TODO Auto-generated method stub
+
+		if(item == null){
+			throw new IllegalArgumentException();
+		}
+		for(Job e: this){
+			if(item.equals(e)){
+				return true;
+			}
+		}
 		return false;
+
 	}
 
 	@Override
 	public Job get(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		if(pos < 0 || pos >= numItems){
+			throw new IndexOutOfBoundsException();
+		}
+		Listnode<Job> curr = head.getNext();
+		for(int i = 0; i < pos; i++){
+			curr = curr.getNext();
+
+		}
+
+
+
+		return curr.getData();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return numItems == 0; //return true if the list is empty 
 	}
 
 	@Override
 	public Job remove(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		if(pos < 0 || pos >= numItems){
+			throw new IndexOutOfBoundsException();
+		}
+
+
+		Listnode<Job> curr = new Listnode<Job>(null);
+		curr = head;
+		for(int i = 0; i < pos; i++){
+			curr = curr.getNext(); 
+		}
+		Job item = curr.getNext().getData();
+		curr.setNext(curr.getNext().getNext());
+
+
+		return item;
 	}
+
+
+
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return numItems;
 	}
 
 }
